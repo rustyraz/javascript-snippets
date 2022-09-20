3cda6b6a4268550e1ea40125cd0f660a98410691
Peter Ngerere <peter.ngerere@medikura.com>
2022-09-20 13:23:07 +0200

    add deep clone array with lodash

--- clone-array.ts
+++ clone-array.ts
@@ -1,3 +1,5 @@
+import cloneDeep from 'lodash.clonedeep'
+
 // Deep cloning of array can be try especially when cloning an array of object
 const items = [
     {
@@ -19,9 +21,17 @@ const items = [
 
 const copy = items.map((item) => {
     return JSON.parse(JSON.stringify(item))
-}) as typeof items // checkout the quick way of passing a reference type to a variable when the return type of the function if not automatically infered
+}) as typeof items // checkout the quick way of type assertion when the return type of the function if not automatically infered
 
 copy[0].category.name = "grocery"
+
+console.log(copy[0].category) // { name: 'grocery' }
+console.log(items[0].category) // { name: 'fruits' }
+
+/* USIND lodash */
+const lodashCopy = cloneDeep(items) // No need for type assertion
+
+lodashCopy[0].category.name = "grocery"
 
 console.log(copy[0].category) // { name: 'grocery' }
 console.log(items[0].category) // { name: 'fruits' }
